c32ab2050c18b87fa9a646ca7fb759514396935d
Nathan Hubbard-Miles <nathanhmiles@gmail.com>
2026-05-25 11:59:31 +0100

    remove isDevMode check for mocked api responses

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import { isDevMode } from '@angular/core';
 
 async function bootstrap() {
-  if (isDevMode()) {
-    const { worker } = await import('./app/mocks/browser');
-    await worker.start({ onUnhandledRequest: 'bypass' });
-  }
+  // Should check isDevMode, removed for demo purposes
+  const { worker } = await import('./app/mocks/browser');
+  await worker.start({ onUnhandledRequest: 'bypass' });
 
   bootstrapApplication(App, appConfig).catch((err) => console.error(err));
 }
