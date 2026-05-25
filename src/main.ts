0f01526b1bfcc3d7e0f58f3866a6aa07f5d0faba
Nathan Hubbard-Miles <nathanhmiles@gmail.com>
2026-05-25 12:17:05 +0100

    attempt fix for msw in github pages

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,15 @@ import { App } from './app/app';
 async function bootstrap() {
   // Should check isDevMode, removed for demo purposes
   const { worker } = await import('./app/mocks/browser');
-  await worker.start({ onUnhandledRequest: 'bypass' });
+
+  const baseHref = document.querySelector('base')?.getAttribute('href') ?? '/';
+
+  return worker.start({
+    serviceWorker: {
+      url: `${baseHref}mockServiceWorker.js`,
+    },
+    onUnhandledRequest: 'bypass',
+  });
 
   bootstrapApplication(App, appConfig).catch((err) => console.error(err));
 }
