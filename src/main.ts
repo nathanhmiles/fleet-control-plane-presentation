175e7ade965ca22aa64fc7528461afdb457a4e0c
Nathan Hubbard-Miles <nathanhmiles@gmail.com>
2026-05-25 12:36:31 +0100

    Revert "attempt to integrate msw and gh-pages"
    This reverts commit ec42909cdf1aea6e99ed6eb8cc8774660bb03c03.

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { isDevMode } from '@angular/core';
 
 async function bootstrap() {
-  // Should check isDevMode, removed for demo purposes
-  const { worker } = await import('./app/mocks/browser');
-
-  const baseHref = document.querySelector('base')?.getAttribute('href') ?? '/';
-
-  return worker.start({
-    serviceWorker: {
-      url: `${baseHref}mockServiceWorker.js`,
-    },
-    onUnhandledRequest: 'bypass',
-  });
+  if (isDevMode()) {
+    const { worker } = await import('./app/mocks/browser');
+    await worker.start({ onUnhandledRequest: 'bypass' });
+  }
 
   bootstrapApplication(App, appConfig).catch((err) => console.error(err));
 }
